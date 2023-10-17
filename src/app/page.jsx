64ae235c0d8b998bfbd39f2d325673ac01d17af9
ViniciusChagas95 <viniciuschagas95@hotmail.com>
2023-10-17 09:52:18 -0300

    Alterações.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,15 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
+    <>
     <h1>Bom dia Brasilllllllll</h1>
+
+    <div>
+      <Link href="/produtos/tenis">TÊNIS</Link> |
+      <Link href="/produtos/produto/tenis-preto:">TÊNIS PRETO</Link>
+    </div>
+    </>
+    
   )
 }
---
+++ src/app/produtos/tenis/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Tenis() {
+    return(
+        <div>
+            <h1>Tênis</h1>
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet ea ad aspernatur dicta aliquam magnam asperiores accusamus. Fugiat, quos repellat. A earum ducimus illo quasi dolorum magnam consequatur saepe autem.
+                </p>
+                <p><Link href={"/"}>Voltar </Link></p>
+            </div>
+        </div>
+    )
+}
